c671915d687acca4f7b6ba03c3c89aee0b3ac2e3
Bohdan Poberezhnyi <poberboh@fit.cvut.cz>
2024-03-30 01:04:54 +0100

    Added dependencies to build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,16 +39,24 @@ android {
 }
 
 dependencies {
+    implementation(libs.material)
 
+    implementation(libs.squareup.okhttp3)
+    implementation(libs.squareup.retrofit)
+    implementation(libs.squareup.retrofit.converter.gson)
+
+    implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
